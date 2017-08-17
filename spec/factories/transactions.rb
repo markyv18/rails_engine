65dfba1176463687c8261a5ffb5f55645a73ce54
Mark Van Akkeren <MarkVanAkkeren@gmail.com>
2017-08-17 14:47:00 -0600

    cleaned up factory girl credit card issue

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :transaction do
-    credit_card_num { Faker::Business.credit_card_number }
+    credit_card_number { Faker::Business.credit_card_number }
     result ["success", "denied", "pending"].sample
     credit_card_expiration_date "2017-08-14"
     invoice
