3771db0af5e5955d5b8a0bb2818ef5e05724d8dc
Wil Donovan <wil.donovan@gmail.com>
2017-08-18 09:35:15 -0600

    deleted unnecessary class method test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,14 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe "instance methods" do
-    describe "#name" do
-      it "should return the Full name of the customer" do
-        customer = Customer.new(first_name: "Harry", last_name: "Boots")
-
-        expected = "Harry Boots"
-
-        expect(customer.name).to eq expected
-      end
-    end
   end
 end
